19f8ec6456f3152b93df6284cddca85ed05cdd39
Andrej Dundovic <andrej@dundovic.com.hr>
2020-02-10 22:37:07 +0100

    updated python scripts with variable nside

--- python/dm-ymw16-skymap.py
+++ python/dm-ymw16-skymap.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 from pyhermes import *
 
-skymap = DMSkymap(nside=64)
+nside = 64
+skymap = DMSkymap(nside)
 gas = YMW16()
 integrator = DMIntegrator(gas)
 integrator.setSunPosition(Vector3QLength(8.3*kiloparsec, 0*parsec, 0*parsec))
@@ -12,11 +13,11 @@ integrator.setSunPosition(Vector3QLength(8.3*kiloparsec, 0*parsec, 0*parsec))
 skymap.setIntegrator(integrator)
 skymap.compute()
 
-output = FITSOutput("!fits/dm-ymw16-skymap.fits.gz")
+output = FITSOutput("!fits/dm-ymw16-skymap-nside{}.fits.gz".format(nside))
 skymap.save(output)
 
 healpy.visufunc.mollview(
         np.log10(np.array(skymap)),
         title='DM of YMW16 log([pc/cm3])', cmap='magma')
 
-plt.savefig('img/dm-ymw16-skymap.png')
+plt.savefig('img/dm-ymw16-skymap-nside{}.png'.format(nside))
--- python/ic-dragon2d-skymap.py
+++ python/ic-dragon2d-skymap.py
@@ -9,22 +9,23 @@ kn_crosssection = KleinNishina()
 CMB_photons = ('cmb', CMB())
 ISRF_photons = ('isrf', ISRF())
 
+nside = 512
 Egamma = 10*GeV
 
 for name, photonfield in (CMB_photons, ISRF_photons):
-    skymap = GammaSkymap(nside=512, Egamma=Egamma)
+    skymap = GammaSkymap(nside=nside, Egamma=Egamma)
     integrator = InverseComptonIntegrator(dragon2D_leptons, photonfield, kn_crosssection)
     integrator.initCacheTable(Egamma, 60, 60, 12)
 
     skymap.setIntegrator(integrator)
     skymap.compute()
 
-    output = FITSOutput("!fits/ic-dragon2d-{}-skymap.fits.gz".format(name))
+    output = FITSOutput("!fits/ic-dragon2d-{}-skymap-nside{}.fits.gz".format(name, nside))
     skymap.save(output)
 
     healpy.visufunc.mollview(
         Egamma*2*np.array(skymap),
-        title='IC with DRAGON2D and {}, nside=512, $E_\gamma = 10\,\mathrm{{ GeV }}$'.format(name.upper()),
+        title='IC with DRAGON2D and {}, nside={}, $E_\gamma = 10\,\mathrm{{ GeV }}$'.format(name.upper(), nside),
         unit="GeV^1 m^-2 s^-1 sr^-1", cmap='magma')
 
-    plt.savefig('img/ic-dragon2d-{}-skymap.png'.format(name))
+    plt.savefig('img/ic-dragon2d-{}-skymap-nside{}.png'.format(name, nside))

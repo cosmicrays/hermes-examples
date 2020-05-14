a0ffc51ce4a3554854fbdfb62f863eb942a59bb7
Andrej Dundovic <andrej@dundovic.com.hr>
2020-05-14 17:31:38 +0200

    e4-gammaray-spectra.py and FITS files

--- python/e4-gammaray-spectra.py
+++ python/e4-gammaray-spectra.py
@@ -7,14 +7,27 @@ import matplotlib.pyplot as plt
 
 name = "gammarays-1GeV-1TeV-{}-{}".format('Fornieri20', 'Remy18')
 
-def integrate_template(integrator, nside, window):
-    
-    integrator.setupCacheTable(100, 100, 20)
+galactic_centre = {
+            'label': 'GC',
+            'mask': RectangularWindow([2*deg, -2*deg], [-2*deg, 2*deg])
+        }
+galactic_plane = {
+            'label': 'GalPlane',
+            'mask': RectangularWindow([2*deg, -2*deg], [-2*deg, 2*deg])
+        }
+intermediate_region = {
+            'label': 'InterLatReg',
+            'mask': RectangularWindow([20*deg, -180*deg], [5*deg, 180*deg])
+        }
+local_region = {
+            'label': 'LocReg',
+            'mask': RectangularWindow([90*deg, -180*deg], [20*deg, 180*deg])
+        }
+
+def skymap_range_template(integrator, nside, mask):
     sun_pos = Vector3QLength(8.0*kpc, 0*pc, 0*pc)
     integrator.setSunPosition(sun_pos)
 
-    mask = RectangularWindow(*window)
-    
     skymap_range = GammaSkymapRange(nside, 1*GeV, 1*TeV, 15)
     skymap_range.setIntegrator(integrator)
     skymap_range.setMask(mask)
@@ -23,44 +36,41 @@ def integrate_template(integrator, nside, window):
     
     return skymap_range
 
-def integrate_pizero(gas, window):
-    nside = 256
+def integrate_pizero(gas, mask):
+    nside = 512
     protons = cosmicrays.Dragon2D(Proton)
     heliums = cosmicrays.Dragon2D(Helium)
     integrator = PiZeroIntegrator([protons, heliums], gas, interactions.Kamae06Gamma())
-    return integrate_template(integrator, nside, window)
+    integrator.setupCacheTable(100, 100, 20)
+    return skymap_range_template(integrator, nside, mask)
 
-def integrate_bremss(gas, window):
-    nside = 256
+def integrate_bremss(gas, mask):
+    nside = 512
     leptons = cosmicrays.Dragon2D([Electron, Positron])
     integrator = PiZeroIntegrator(leptons, gas, interactions.BremsstrahlungSimple())
-    return integrate_template(integrator, nside, window)
+    integrator.setupCacheTable(100, 100, 20)
+    return skymap_range_template(integrator, nside, mask)
 
-def integrate_IC(window):
+def integrate_IC(mask):
     nside = 128
-    leptons = Dragon2DCRDensity([Electron, Positron])
+    leptons = cosmicrays.Dragon2D([Electron, Positron])
     integrator = InverseComptonIntegrator(leptons, photonfields.ISRF(), interactions.KleinNishina())    
-    return integrate_template(integrator, nside, window)
+    integrator.setupCacheTable(50, 50, 10)
+    return skymap_range_template(integrator, nside, mask)
 
 # Neutral gas contained in the so-called ring model
 neutral_gas_HI = neutralgas.RingModel(neutralgas.RingType.HI)
 neutral_gas_CO = neutralgas.RingModel(neutralgas.RingType.CO)
 
-galactic_centre = {
-            'label': 'GC',
-            'window': ([2*deg, -2*deg], [-2*deg, 2*deg])
-        }
-galactic_plane = {
-            'label': 'GalPlane',
-            'window': ([5*deg, 0*deg], [0*deg, 180*deg])
-        }
+window = intermediate_region #galactic_plane #galactic_centre
 
-window = galactic_plane
+skymap_range = integrate_pizero(neutral_gas_HI, window['mask'])
+skymap_range.save(outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-HI', window['label'])))
 
-skymap_range_pi0_HI = integrate_pizero(neutral_gas_HI, window['window'])
-output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-HI', window['label']))
-skymap_range_pi0_HI.save(output_hp)
+skymap_range = integrate_pizero(neutral_gas_CO, window['mask'])
+output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-CO', window['label']))
+skymap_range.save(output_hp)
 
-skymap_range_pi0_CO = integrate_pizero(neutral_gas_CO, window['window'])
-output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-C0', window['label']))
-skymap_range_pi0_CO.save(output_hp)
+skymap_range = integrate_IC(window['mask'])
+output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'IC', window['label']))
+skymap_range.save(output_hp)

518406e7dc61daf1fa0b8eae9ec68f8ea7d0cc1f
Andrej Dundovic <andrej.dundovic@desy.de>
2020-05-25 13:04:15 +0200

    Update e4-gamma-spectra to the latest version before Carmelo's comments

--- python/e4-gammaray-spectra.py
+++ python/e4-gammaray-spectra.py
@@ -9,11 +9,11 @@ name = "gammarays-1GeV-1TeV-{}-{}".format('Fornieri20', 'Remy18')
 
 galactic_centre = {
             'label': 'GC',
-            'mask': RectangularWindow(latitude=[-2*deg, 2*deg], longitude=[2*deg, -2*deg])
+            'mask': RectangularWindow(latitude=[2*deg, -2*deg], longitude=[358*deg, 2*deg])
         }
 galactic_plane = {
             'label': 'GalPlane',
-            'mask': RectangularWindow(latitude=[0*deg, 5*deg], longitude=[0*deg, 360*deg])
+            'mask': RectangularWindow(latitude=[5*deg, 0*deg], longitude=[0*deg, 360*deg])
         }
 intermediate_region = {
             'label': 'InterLatReg',
@@ -28,7 +28,7 @@ def skymap_range_template(integrator, nside, mask):
     sun_pos = Vector3QLength(8.0*kpc, 0*pc, 0*pc)
     integrator.setSunPosition(sun_pos)
 
-    skymap_range = GammaSkymapRange(nside, 1*GeV, 1*TeV, 15)
+    skymap_range = GammaSkymapRange(nside, 1*GeV, 1*TeV, 25)
     skymap_range.setIntegrator(integrator)
     skymap_range.setMask(mask)
     
@@ -55,30 +55,30 @@ def integrate_IC(mask):
     nside = 128
     leptons = cosmicrays.Dragon2D([Electron, Positron])
     integrator = InverseComptonIntegrator(leptons, photonfields.ISRF(), interactions.KleinNishina())    
-    integrator.setupCacheTable(50, 50, 10)
+    integrator.setupCacheTable(60, 60, 12)
     return skymap_range_template(integrator, nside, mask)
 
 # Neutral gas contained in the so-called ring model
 neutral_gas_HI = neutralgas.RingModel(neutralgas.RingType.HI)
 neutral_gas_CO = neutralgas.RingModel(neutralgas.RingType.CO)
 
-window = local_region #galactic_centre #intermediate_region #galactic_plane #galactic_centre
+for window in [galactic_centre, galactic_plane, intermediate_region, local_region]:
 
-#skymap_range = integrate_pizero(neutral_gas_HI, window['mask'])
-#skymap_range.save(outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-HI', window['label'])))
+    skymap_range = integrate_pizero(neutral_gas_HI, window['mask'])
+    skymap_range.save(outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-HI', window['label'])))
 
-#skymap_range = integrate_pizero(neutral_gas_CO, window['mask'])
-#output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-CO', window['label']))
-#skymap_range.save(output_hp)
+    skymap_range = integrate_pizero(neutral_gas_CO, window['mask'])
+    output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'pi0-CO', window['label']))
+    skymap_range.save(output_hp)
 
-#skymap_range = integrate_IC(window['mask'])
-#output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'IC', window['label']))
-#skymap_range.save(output_hp)
+    skymap_range = integrate_IC(window['mask'])
+    output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'IC', window['label']))
+    skymap_range.save(output_hp)
 
-skymap_range = integrate_bremss(neutral_gas_HI, window['mask'])
-output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'brems-HI', window['label']))
-skymap_range.save(output_hp)
+    skymap_range = integrate_bremss(neutral_gas_HI, window['mask'])
+    output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'brems-HI', window['label']))
+    skymap_range.save(output_hp)
 
-#skymap_range = integrate_bremss(neutral_gas_CO, window['mask'])
-#output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'brems-CO', window['label']))
-#skymap_range.save(output_hp)
+    skymap_range = integrate_bremss(neutral_gas_CO, window['mask'])
+    output_hp = outputs.HEALPixFormat("!fits/{}-{}-{}.fits.gz".format(name, 'brems-CO', window['label']))
+    skymap_range.save(output_hp)

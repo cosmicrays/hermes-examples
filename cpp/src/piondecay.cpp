3727d5365c9a0b0f9fd96c4572159f3a3b3e7acf
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-05-27 15:23:47 +0200

    fixed piondecay routines

--- cpp/src/piondecay.cpp
+++ cpp/src/piondecay.cpp
@@ -8,31 +8,29 @@ namespace hermes {
 void examplePion() {
 
 	// cosmic ray density models
-	auto simpleModel = std::make_shared<SimpleCRDensity>(SimpleCRDensity());
-	std::vector<PID> particletypes = {Proton};
-	auto dragonModel = std::make_shared<Dragon2DCRDensity>(Dragon2DCRDensity(
-				getDataPath("CosmicRays/Gaggero17/run_2D.fits.gz"),
-				particletypes)); 
+    auto simpleModel = std::make_shared<cosmicrays::SimpleCR>();
+    std::vector<PID> particletypes = {Proton};
+    auto dragonFilename = getDataPath("CosmicRays/Fornieri20/run2d_gamma_D03,7_delta0,45_vA13.fits.gz");
+    auto dragonModel = std::make_shared<cosmicrays::Dragon2D>(dragonFilename, particletypes);
 
 	// interaction
-	auto kamae = std::make_shared<Kamae06>(Kamae06());
+    auto kamae = std::make_shared<interactions::Kamae06Gamma>();
 
 	// HI model
-	auto ringModel = std::make_shared<RingModelDensity>(RingModelDensity());
+    auto ringModel = std::make_shared<neutralgas::RingModel>(neutralgas::GasType::HI);
 	
-	// integrator
-	auto intPiZero = std::make_shared<PiZeroIntegrator>(
-		PiZeroIntegrator(dragonModel, ringModel, kamae));
+    // integrator
+    auto integrator = std::make_shared<PiZeroIntegrator>(dragonModel, ringModel, kamae);
 
-	// skymap
-	int nside = 32;
-	auto skymap = std::make_shared<GammaSkymap>(GammaSkymap(nside, 1_GeV));
-	skymap->setIntegrator(intPiZero);
+    // skymap
+    int nside = 32;
+    auto skymap = std::make_shared<GammaSkymap>(GammaSkymap(nside, 1_GeV));
+    skymap->setIntegrator(integrator);
 
-	auto output = std::make_shared<FITSOutput>(FITSOutput("!example-pion.fits.gz"));
-	
-	skymap->compute();
-	skymap->save(output);
+    auto output = std::make_shared<outputs::HEALPixFormat>("!example-ic.fits.gz");
+
+    skymap->compute();
+    skymap->save(output);
 }
 
 } // namespace hermes

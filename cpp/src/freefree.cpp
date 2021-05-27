f1aa6c8618ffe477f29d45bd30b26bb85cdb0c7a
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-05-27 14:50:37 +0200

    fixed freefree with the new HERMES

--- cpp/src/freefree.cpp
+++ cpp/src/freefree.cpp
@@ -7,22 +7,22 @@ namespace hermes {
 
 void exampleFreeFree() {
 	// gas models
-	auto gasCordes91 = std::make_shared<ionizedgas::HII_Cordes91>();
+    auto gasCordes91 = std::make_shared<ionizedgas::HII_Cordes91>();
     auto gasYMW16 = std::make_shared<ionizedgas::YMW16>();
 	
 	// integrator
     auto intFreeFree = std::make_shared<FreeFreeIntegrator>(FreeFreeIntegrator(gasYMW16));
 
 	// skymap
-	//int nside = 32;
-	//auto skymaps = std::make_shared<RadioSkymapRange>(RadioSkymapRange(nside, 10_MHz, 10_GHz, 10));
+	int nside = 32;
+	auto skymaps = std::make_shared<RadioSkymapRange>(RadioSkymapRange(nside, 10_MHz, 10_GHz, 10));
     
-//	skymaps->setIntegrator(intFreeFree);
-//	skymaps->compute();
-//
-//	// save
-//	auto output = std::make_shared<FITSOutput>(FITSOutput("!example-freefree.fits.gz"));
-//	skymaps->save(output);
+    skymaps->setIntegrator(intFreeFree);
+    skymaps->compute();
+
+	// save
+    auto output = std::make_shared<outputs::HEALPixFormat>("!example-freefree.fits.gz");
+	skymaps->save(output);
 }
 
 } // namespace hermes

138119538a41368cafb2fe168ca80d09ed3c2d98
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-05-27 15:23:30 +0200

    fixed inversecompton routines

--- cpp/src/inversecompton.cpp
+++ cpp/src/inversecompton.cpp
@@ -7,45 +7,41 @@ namespace hermes {
 
 void exampleIC() {
 
-	// cosmic ray density models
-	auto simpleModel = std::make_shared<SimpleCRDensity>(SimpleCRDensity());
-	std::vector<PID> particletypes = {Proton};
-	auto dragonModel = std::make_shared<Dragon2DCRDensity>(Dragon2DCRDensity(
-				getDataPath("CosmicRays/Gaggero17/run_2D.fits.gz"),
-				particletypes)); 
+    // cosmic ray density models
+    auto simpleModel = std::make_shared<cosmicrays::SimpleCR>();
+    std::vector<PID> particletypes = {Proton};
+    auto dragonFilename = getDataPath("CosmicRays/Fornieri20/run2d_gamma_D03,7_delta0,45_vA13.fits.gz");
+    auto dragonModel = std::make_shared<cosmicrays::Dragon2D>(dragonFilename, particletypes);
 
-	// photon field
-	auto cmb = std::make_shared<CMB>(CMB());
-	auto isrf = std::make_shared<ISRF>(ISRF());
+    // photon field
+    auto cmb = std::make_shared<photonfields::CMB>();
+    auto isrf = std::make_shared<photonfields::ISRF>();
 
-	// interaction
-        auto kleinnishina = std::make_shared<KleinNishina>(KleinNishina());
+    // interaction
+    auto kleinnishina = std::make_shared<interactions::KleinNishina>();
 
-        // integrator
-        auto intIC = std::make_shared<InverseComptonIntegrator>(
-                InverseComptonIntegrator(dragonModel, isrf, kleinnishina));
-        
-        // skymap
-        int nside = 32;
-        auto mask = std::make_shared<RectangularWindow>(RectangularWindow(
-                        QAngle(8_deg), QAngle(-8_deg), QAngle(-80_deg), QAngle(80_deg)));
-        auto skymaps = std::make_shared<GammaSkymap>(GammaSkymap(nside, 100_MeV));
-        //auto skymaps = std::make_shared<GammaSkymapRange>(GammaSkymapRange(nside, 100_MeV, 300_GeV, 10));
-        skymaps->setMask(mask);
-        skymaps->setIntegrator(intIC);
+    // integrator
+    auto intIC = std::make_shared<InverseComptonIntegrator>(dragonModel, isrf, kleinnishina);
 
-	auto output = std::make_shared<FITSOutput>(FITSOutput("!example-ic.fits.gz"));
-	
-	skymaps->compute();
-	skymaps->save(output);
+    // skymap
+    int nside = 32;
+    auto mask = std::make_shared<RectangularWindow>(std::array<QAngle, 2> {-8_deg, 8_deg}, std::array<QAngle, 2> {-80_deg, 80_deg});
+    auto skymaps = std::make_shared<GammaSkymap>(GammaSkymap(nside, 100_MeV));
+    skymaps->setMask(mask);
+    skymaps->setIntegrator(intIC);
+
+    auto output = std::make_shared<outputs::HEALPixFormat>("!example-ic.fits.gz");
+
+    skymaps->compute();
+    skymaps->save(output);
 }
 
 } // namespace hermes
 
 int main(void){
 
-	hermes::exampleIC();
+    hermes::exampleIC();
 
-	return 0;
+    return 0;
 }
 
